3d2929bed07ca6170f5a06feef6857f3b6602a8e
Felipe Miguel dos Santos <felipe.miguel@csptech.com.br>
2023-06-03 15:07:13 -0300

    feat: Google drive

--- src/Modules/OrderService/services.service.ts
+++ src/Modules/OrderService/services.service.ts
@@ -111,7 +111,42 @@ export class ServiceService {
     }
   }
 
-  async deleteFileByStatusFolder(status: string) {}
+  async deleteFileByStatusFolder(
+    folderName: string,
+    idFolderClientName: string,
+    filename: string,
+  ) {
+    try {
+      const result = await listFolder({
+        parents: idFolderClientName,
+      })
+      const resultFoldersName = result.files.filter(
+        (folder) => folder.name === folderName,
+      )
+      if (resultFoldersName.length) {
+        const {id} = resultFoldersName[0]
+        /** Lista todos os arquivos dentro da pasta */
+        const resultList = await list({parents: id})
+        resultList.files.forEach(async (file) => {
+          if (file.name === `${filename}.pdf`) {
+            console.log(
+              `[Sistema] - Arquivo ${file.name} excluido com sucesso dentro da pasta ${folderName}`,
+            )
+            /** Delete o arquivo */
+            await destroy({fileId: file.id})
+          }
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      throw new HttpException(
+        {
+          message: error,
+        },
+        HttpStatus.EXPECTATION_FAILED,
+      )
+    }
+  }
 
   async uploadFile(idFolder: string, fileName: string, status: string) {
     try {
@@ -124,25 +159,29 @@ export class ServiceService {
       if (!resultFilderFileName.length) {
         /** Quando não encontrar o arquivo */
         /** Fazer o upload do arquivo */
+        console.log(
+          `[Sistema] - Fazendo o upload do arquivo ${fileName} no Google drive...`,
+        )
         await uploadFile({
           fileName: `${fileName}.pdf`,
           filePath: `${filePath}.pdf`,
           parents: idFolder,
         })
-        await this.deleteFileByStatusFolder(status)
         await this.deleteFile(fileName)
       } else {
         /** Quando encontrar o arquivo */
         /** Excluir o arquivo e fazer o upload do novo arquivo */
         const {id} = resultFilderFileName[0]
         await destroy({fileId: id})
+        console.log(
+          `[Sistema] - Fazendo o upload do arquivo ${fileName} no Google drive...`,
+        )
         await uploadFile({
           fileName: `${fileName}.pdf`,
           filePath: `${filePath}.pdf`,
           parents: idFolder,
         })
         await this.deleteFile(fileName)
-        await this.deleteFileByStatusFolder(status)
       }
     } catch (error) {
       console.log(error)
@@ -202,15 +241,24 @@ export class ServiceService {
   ) {
     if (typeDocument === 'ORCAMENTO') {
       /** Cria a pasta ORCAMENTOS */
+      console.log(`[Sistema] - Verificando se pasta ORÇAMENTOS já existe...`)
       await this.createFolder(idFolder, 'ORÇAMENTOS', filename, status)
     } else {
       if (status === 'PAGO') {
         /** Cria a pasta O.S PAGAS */
+        console.log(`[Sistema] - Verificando se pasta O.S PAGAS já existe...`)
         await this.createFolder(idFolder, 'O.S PAGAS', filename, status)
+        /** Deleta o arquivo dentro da pasta O.S PENDENTES para evitar ficar em duas pastas ao mesmo tempo*/
+        await this.deleteFileByStatusFolder('O.S PENDENTES', idFolder, filename)
       }
       if (status === 'PENDENTE') {
         /** Cria a pasta O.S PENDENTES */
+        console.log(
+          `[Sistema] - Verificando se pasta O.S PENDENTES já existe...`,
+        )
         await this.createFolder(idFolder, 'O.S PENDENTES', filename, status)
+        /** Deleta o arquivo dentro da pasta O.S PENDENTES para evitar ficar em duas pastas ao mesmo tempo*/
+        await this.deleteFileByStatusFolder('O.S PAGAS', idFolder, filename)
       }
     }
   }
@@ -235,16 +283,19 @@ export class ServiceService {
     /** Se a pasta do cliente não existir */
     if (!resultFolderClients.length) {
       /** Cria a pasta com o nome do cliente */
+      console.log(
+        `[Sistema] - Criando a pasta com o nome do cliente: ${clientName}`,
+      )
       const {data} = await createFolder({
         folderName: clientName,
         parents: id,
       })
-      const idClient = data?.id
+      const idFolderWithNameClient = data?.id
       /** Cria as pasta para salvar o documento de acordo com o status */
       await this.createFoldersToDocuments(
         status,
         typeDocument,
-        idClient,
+        idFolderWithNameClient,
         filename,
       )
     } else {
@@ -268,18 +319,18 @@ export class ServiceService {
     const ID_FOLDER_MAIN = process.env.ID_FOLDER_MAIN_GOOGLE_DRIVE
 
     try {
-      /** Verifica se a pasta CLIENTES já existe */
+      console.log('[Sistema] - Verificando se a pasta CLIENTES já existe...')
       const listResult = await listFolder({
         parents: ID_FOLDER_MAIN,
       })
       /** Se não existir a pasta CLIENTES */
       if (!listResult.files.length) {
-        /** Cria a pasta CLIENTES */
+        console.log('[Sistema] - Criando a pasta CLIENTES...')
         const {data} = await createFolder({
           folderName: 'CLIENTES',
           parents: ID_FOLDER_MAIN,
         })
-        /** Faz o upload dentro da pasta */
+        /** Cria a pasta com o nome do cliente */
         await this.createFolderWithClientName(
           data?.id,
           clientName,
@@ -294,7 +345,7 @@ export class ServiceService {
         )
         if (resultFilterFoldersName.length) {
           const {id} = resultFilterFoldersName[0]
-          /** Faz o upload dentro da pasta */
+          /** Cria a pasta com o nome do cliente */
           await this.createFolderWithClientName(
             id,
             clientName,
@@ -325,7 +376,6 @@ export class ServiceService {
         console.error('Erro ao excluir o arquivo:', error)
         return
       }
-      console.log('Arquivo excluído com sucesso!')
     })
   }
 
@@ -348,6 +398,7 @@ export class ServiceService {
       /**
        * @description Converte o base64 em arquivo .pdf
        */
+      console.log('[Sistema] - Salvando o arquivo .pdf no servidor...')
       new Promise<void>((resolve, reject) => {
         fs.writeFile(
           filePath.concat('.pdf'),
@@ -362,14 +413,18 @@ export class ServiceService {
           },
         )
       })
-      return await this.saveFileInFolderGoogleDrive(
+      console.log(
+        '[Sistema] - Iniciando o processo de upload no Google Drive...',
+      )
+      await this.saveFileInFolderGoogleDrive(
         clientName,
         status,
         typeDocument,
         filename,
       )
+      console.log(`[Sistema] - Procedimento finalizado com sucesso.`)
     } catch (error) {
-      console.log(error)
+      console.log(`[Sistema] - Houve um erro: ${error}`)
       throw new HttpException(
         {
           message: error,
